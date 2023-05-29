e66500074aefe7126e1c5b0bd3a70facc20cea81
Markus Ineichen <markus.ineichen1@sluz.ch>
2023-05-29 16:20:56 +0200

    new file:   TestRunner.html 	new file:   js/lib/qunit/qunit.js 	new file:   js/src/ArrayHelper.js 	new file:   js/test/ArrayHelperTest.js 	new file:   style/qunit/qunit.css

---
+++ js/src/ArrayHelper.js
@@ -0,0 +1,12 @@
+// Objekt mit Objektmethoden unter Verwendung der Objekt-Literal-Schreibweise
+/* Seit ES2015 können Sie bei Verwendung der Objekt-Literal-Schreibweise 
+bei der Definition von Objektmethoden das Schlüsselwort function und den 
+vorausgehenden Doppelpunkt auch weglassen. */
+const ArrayHelper = {
+  summe: function(array) {
+    // TODO
+  },
+  produkt(array) {
+    // TODO
+  },
+};
